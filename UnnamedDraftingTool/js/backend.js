ffffcbbd8517f71e00710ac9404eac6bcefb8c0a
aCertainProgrammer <a_certain_programmer@proton.me>
2024-10-30 19:41:28 +0100

    clone UnnamedDraftingTool instead of copypasting it (xD)

--- UnnamedDraftingTool/js/backend.js
+++ UnnamedDraftingTool/js/backend.js
@@ -1,51 +1,51 @@
 class Backend {
-  constructor() { }
-  requestVisibleChampions(request) {
-    let data = [];
-    switch (request.team) {
-      case "all":
-        data = all_champions_data;
-        break;
-      case "leo":
-        data = leo_data;
-        break;
-      case "enemy":
-        data = enemy_data;
-        break;
-    }
-    if (request.role == "all") {
-      data = data["top"].concat(
-        data["jungle"],
-        data["mid"],
-        data["adc"],
-        data["support"],
-      );
-      data = this.sortAndRemoveDuplicates(data);
-    } else {
-      data = this.sortAndRemoveDuplicates(data[request.role]);
-    }
-    data = this.filterDataBySearchQuery(data, request.search);
-    return data;
-  }
-  sortAndRemoveDuplicates(data) {
-    data.sort();
-    const newData = [];
-    newData.push(data[0]);
-    for (let i = 1; i < data.length; i++) {
-      if (data[i - 1] != data[i]) {
-        newData.push(data[i]);
-      }
-    }
-    return newData;
-  }
-  filterDataBySearchQuery(data, search_query) {
-    if (search_query == "") return data;
-    const newData = [];
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].toLowerCase().includes(search_query.toLowerCase())) {
-        newData.push(data[i]);
-      }
-    }
-    return newData;
-  }
+	constructor() {}
+	requestVisibleChampions(request) {
+		let data = [];
+		switch (request.team) {
+			case "all":
+				data = all_champions_data;
+				break;
+			case "leo":
+				data = leo_data;
+				break;
+			case "enemy":
+				data = enemy_data;
+				break;
+		}
+		if (request.role == "all") {
+			data = data["top"].concat(
+				data["jungle"],
+				data["mid"],
+				data["adc"],
+				data["support"],
+			);
+			data = this.sortAndRemoveDuplicates(data);
+		} else {
+			data = this.sortAndRemoveDuplicates(data[request.role]);
+		}
+		data = this.filterDataBySearchQuery(data, request.search);
+		return data;
+	}
+	sortAndRemoveDuplicates(data) {
+		data.sort();
+		const newData = [];
+		newData.push(data[0]);
+		for (let i = 1; i < data.length; i++) {
+			if (data[i - 1] != data[i]) {
+				newData.push(data[i]);
+			}
+		}
+		return newData;
+	}
+	filterDataBySearchQuery(data, search_query) {
+		if (search_query == "") return data;
+		const newData = [];
+		for (let i = 0; i < data.length; i++) {
+			if (data[i].toLowerCase().includes(search_query.toLowerCase())) {
+				newData.push(data[i]);
+			}
+		}
+		return newData;
+	}
 }
--- UnnamedDraftingTool/js/frontend.js
+++ UnnamedDraftingTool/js/frontend.js
@@ -1,152 +1,160 @@
 backend = new Backend();
 class Frontend {
-  constructor() {
-    this.request = {
-      team: "all",
-      role: "all",
-      search: "",
-    };
-    this.renderingData = {
-      visibleChampions: [],
-      pickedChampions: [],
-      bannedChampions: [],
-    };
-    this.selectedChampion = "";
-    this.picks = document.querySelectorAll(".champion-pick");
-    this.bans = document.querySelectorAll(".champion-ban");
-    this.picks.forEach((current) => {
-      current.addEventListener("click", this.placeChampion);
-      current.childNodes[1].dataset.champion = "";
-    });
-    this.bans.forEach((current) => {
-      current.addEventListener("click", this.placeChampion);
-      current.childNodes[1].dataset.champion = "";
-    });
+	constructor() {
+		this.request = {
+			team: "all",
+			role: "all",
+			search: "",
+		};
+		this.renderingData = {
+			visibleChampions: [],
+			pickedChampions: [],
+			bannedChampions: [],
+		};
+		this.selectedChampion = "";
+		this.picks = document.querySelectorAll(".champion-pick");
+		this.bans = document.querySelectorAll(".champion-ban");
+		this.picks.forEach((current) => {
+			current.addEventListener("click", this.placeChampion);
+			current.childNodes[1].dataset.champion = "";
+		});
+		this.bans.forEach((current) => {
+			current.addEventListener("click", this.placeChampion);
+			current.childNodes[1].dataset.champion = "";
+		});
 
-    this.championsContainer = document.querySelector("#champions-container");
-    this.logos = document.querySelectorAll(".team-logo");
-    this.logos.forEach((current) => {
-      current.addEventListener("click", () => {
-        this.request.team = current.id;
-        this.render();
-      });
-    });
+		this.championsContainer = document.querySelector(
+			"#champions-container",
+		);
+		this.logos = document.querySelectorAll(".team-logo");
+		this.logos.forEach((current) => {
+			current.addEventListener("click", () => {
+				this.request.team = current.id;
+				this.render();
+			});
+		});
 
-    this.roleIcons = document.querySelectorAll(".role-icon");
-    this.roleIcons.forEach((current) => {
-      current.addEventListener("click", () => {
-        if (this.request.role == current.id) {
-          this.request.role = "all";
-        } else if (this.request.role != current.id)
-          this.request.role = current.id;
-        this.render();
-      });
-    });
-    this.searchBar = document.querySelector(".search-bar");
-    this.searchBar.addEventListener("input", () => {
-      frontend.request.search = frontend.searchBar.value;
-      frontend.render();
-    });
-  }
+		this.roleIcons = document.querySelectorAll(".role-icon");
+		this.roleIcons.forEach((current) => {
+			current.addEventListener("click", () => {
+				if (this.request.role == current.id) {
+					this.request.role = "all";
+				} else if (this.request.role != current.id)
+					this.request.role = current.id;
+				this.render();
+			});
+		});
+		this.searchBar = document.querySelector(".search-bar");
+		this.searchBar.addEventListener("input", () => {
+			frontend.request.search = frontend.searchBar.value;
+			frontend.render();
+		});
+	}
 
-  render() {
-    this.renderingData.visibleChampions = backend.requestVisibleChampions(
-      this.request,
-    );
-    this.requestPickedChampions();
-    this.requestBannedChampions();
-    this.renderVisibleChampions();
-    this.renderPickedChampions();
-    this.renderBannedChampions();
-  }
+	render() {
+		this.renderingData.visibleChampions = backend.requestVisibleChampions(
+			this.request,
+		);
+		this.requestPickedChampions();
+		this.requestBannedChampions();
+		this.renderVisibleChampions();
+		this.renderPickedChampions();
+		this.renderBannedChampions();
+	}
 
-  renderVisibleChampions() {
-    while (this.championsContainer.hasChildNodes()) {
-      this.championsContainer.removeChild(this.championsContainer.firstChild);
-    }
-    for (let i = 0; i < this.renderingData.visibleChampions.length; i++) {
-      const newChampion = this.renderingData.visibleChampions[i];
-      const newNode = document.createElement("div");
-      newNode.classList += "champion-container";
-      const championIcon = document.createElement("img");
-      championIcon.classList += "champion-icon";
-      championIcon.src =
-        "./img/champion_icons/tiles/" + capitalize(newChampion) + "_0.jpg";
-      championIcon.alt = this.renderingData.visibleChampions[i];
-      championIcon.dataset.champion = newChampion;
-      if (
-        this.renderingData.pickedChampions.includes(newChampion) ||
-        this.renderingData.bannedChampions.includes(newChampion)
-      ) {
-        championIcon.style.opacity = "0.4";
-      }
-      newNode.appendChild(championIcon);
-      this.championsContainer.appendChild(newNode);
-      championIcon.addEventListener("click", this.selectChampion);
-    }
-  }
-  requestPickedChampions() {
-    for (let i = 0; i < 10; i++) {
-      let img = this.picks[i].childNodes[1];
-      this.renderingData.pickedChampions[i] = img.dataset.champion;
-    }
-  }
-  requestBannedChampions() {
-    for (let i = 0; i < 10; i++) {
-      let img = this.bans[i].childNodes[1];
-      this.renderingData.bannedChampions[i] = img.dataset.champion;
-    }
-  }
-  renderPickedChampions() {
-    for (let i = 0; i < 10; i++) {
-      let img = this.picks[i].childNodes[1];
-      if (this.renderingData.pickedChampions[i] == "") {
-        img.src = "./img/pick_icon.png";
-        img.dataset.champion = "";
-        continue;
-      }
-      img.src =
-        "./img/champion_icons/centered/" +
-        capitalize(this.renderingData.pickedChampions[i]) +
-        "_0.jpg";
-      img.dataset.champion = this.renderingData.pickedChampions[i];
-    }
-  }
-  renderBannedChampions() {
-    for (let i = 0; i < 10; i++) {
-      let img = this.bans[i].childNodes[1];
-      if (this.renderingData.bannedChampions[i] == "") {
-        img.src = "./img/pick_icon.png";
-        img.dataset.champion = "";
-        continue;
-      }
-      img.src =
-        "./img/champion_icons/tiles/" +
-        capitalize(this.renderingData.bannedChampions[i]) +
-        "_0.jpg";
-      img.dataset.champion = this.renderingData.bannedChampions[i];
-    }
-  }
-  //calling frontend. instead of this. is necessary due to how the "this"
-  //keyword works in javascript
-  selectChampion(event) {
-    console.log(frontend.renderingData.pickedChampions);
-    frontend.selectedChampion = event.target.dataset.champion;
-    if (
-      frontend.renderingData.pickedChampions.includes(
-        frontend.selectedChampion,
-      ) ||
-      frontend.renderingData.bannedChampions.includes(frontend.selectedChampion)
-    ) {
-      frontend.selectedChampion = "";
-    }
-  }
-  placeChampion(event) {
-    if (frontend.selectedChampion == "") {
-      event.target.src = "./img/pick_icon.png";
-    }
-    event.target.dataset.champion = frontend.selectedChampion;
-    frontend.selectedChampion = "";
-    frontend.render();
-  }
+	renderVisibleChampions() {
+		while (this.championsContainer.hasChildNodes()) {
+			this.championsContainer.removeChild(
+				this.championsContainer.firstChild,
+			);
+		}
+		for (let i = 0; i < this.renderingData.visibleChampions.length; i++) {
+			const newChampion = this.renderingData.visibleChampions[i];
+			const newNode = document.createElement("div");
+			newNode.classList += "champion-container";
+			const championIcon = document.createElement("img");
+			championIcon.classList += "champion-icon";
+			championIcon.src =
+				"./img/champion_icons/tiles/" +
+				capitalize(newChampion) +
+				"_0.jpg";
+			championIcon.alt = this.renderingData.visibleChampions[i];
+			championIcon.dataset.champion = newChampion;
+			if (
+				this.renderingData.pickedChampions.includes(newChampion) ||
+				this.renderingData.bannedChampions.includes(newChampion)
+			) {
+				championIcon.style.opacity = "0.4";
+			}
+			newNode.appendChild(championIcon);
+			this.championsContainer.appendChild(newNode);
+			championIcon.addEventListener("click", this.selectChampion);
+		}
+	}
+	requestPickedChampions() {
+		for (let i = 0; i < 10; i++) {
+			let img = this.picks[i].childNodes[1];
+			this.renderingData.pickedChampions[i] = img.dataset.champion;
+		}
+	}
+	requestBannedChampions() {
+		for (let i = 0; i < 10; i++) {
+			let img = this.bans[i].childNodes[1];
+			this.renderingData.bannedChampions[i] = img.dataset.champion;
+		}
+	}
+	renderPickedChampions() {
+		for (let i = 0; i < 10; i++) {
+			let img = this.picks[i].childNodes[1];
+			if (this.renderingData.pickedChampions[i] == "") {
+				img.src = "./img/pick_icon.png";
+				img.dataset.champion = "";
+				continue;
+			}
+			img.src =
+				"./img/champion_icons/centered/" +
+				capitalize(this.renderingData.pickedChampions[i]) +
+				"_0.jpg";
+			img.dataset.champion = this.renderingData.pickedChampions[i];
+		}
+	}
+	renderBannedChampions() {
+		for (let i = 0; i < 10; i++) {
+			let img = this.bans[i].childNodes[1];
+			if (this.renderingData.bannedChampions[i] == "") {
+				img.src = "./img/pick_icon.png";
+				img.dataset.champion = "";
+				continue;
+			}
+			img.src =
+				"./img/champion_icons/tiles/" +
+				capitalize(this.renderingData.bannedChampions[i]) +
+				"_0.jpg";
+			img.dataset.champion = this.renderingData.bannedChampions[i];
+		}
+	}
+	//calling frontend. instead of this. is necessary due to how the "this"
+	//keyword works in javascript
+	selectChampion(event) {
+		console.log(frontend.renderingData.pickedChampions);
+		frontend.selectedChampion = event.target.dataset.champion;
+		if (
+			frontend.renderingData.pickedChampions.includes(
+				frontend.selectedChampion,
+			) ||
+			frontend.renderingData.bannedChampions.includes(
+				frontend.selectedChampion,
+			)
+		) {
+			frontend.selectedChampion = "";
+		}
+	}
+	placeChampion(event) {
+		if (frontend.selectedChampion == "") {
+			event.target.src = "./img/pick_icon.png";
+		}
+		event.target.dataset.champion = frontend.selectedChampion;
+		frontend.selectedChampion = "";
+		frontend.render();
+	}
 }
